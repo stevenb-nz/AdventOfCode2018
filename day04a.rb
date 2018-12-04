a87fd308bab817fbe2cb2ac4d618209a214eca08
Steven Brown <sgbrown@mac.com>
2018-12-04 22:10:06 +1300

    day 4 solutions

---
+++ day04a.rb
@@ -0,0 +1,34 @@
+def day04(input)
+
+  input = input.split(/\n/).sort
+
+  minutes = Hash.new
+  input.each_with_index do |line,i|
+    if line[19,5] == "Guard" then
+      guardnum = line[26..-13].to_i
+      if !minutes.has_key?(guardnum) then
+        minutes[guardnum] = Array.new(60,0)
+      end
+      j = i+1
+      if j < input.count then
+        while input[j][19,5] != "Guard"
+          for k in input[j][15,2].to_i...input[j+1][15,2].to_i do
+            minutes[guardnum][k] += 1
+          end
+          j += 2
+          if j == input.count then
+            break
+          end
+        end
+      end
+    end
+  end
+
+  guardminutes = minutes.max_by{|k,v| v.inject :+}
+
+  return guardminutes[0] * guardminutes[1].index(guardminutes[1].max)
+
+end
+
+input = File.read("day04_input.txt").chomp
+puts day04(input)
