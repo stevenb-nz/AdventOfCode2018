779680c6157ee0387247299e6bd94ab19f12a593
Steven Brown <sgbrown@mac.com>
2018-12-07 20:15:23 +1300

    day 7 part A solution

---
+++ day07a.rb
@@ -0,0 +1,58 @@
+def day07(input)
+
+  input = input.split(/\n/)
+
+  deps = Hash.new
+  input.each do |line|
+    if deps.has_key?(line[5]) then
+      deps[line[5]].push line[-12]
+    else
+      deps[line[5]] = [line[-12]]
+    end
+  end
+  peds = Hash.new
+  input.each do |line|
+    if peds.has_key?(line[-12]) then
+      peds[line[-12]].push line[5]
+    else
+      peds[line[-12]] = [line[5]]
+    end
+  end
+  candidates = []
+  peds.each do |a|
+    a[1].each do |d|
+      if !peds.has_key?(d) then
+        candidates.append d
+      end
+    end
+  end
+
+  result = ""
+  nextletter = candidates.min
+
+  while deps.has_key?(nextletter)
+    result = result + nextletter
+    candidates.delete nextletter
+    peds.each_key do |newcandidate|
+      if !result.include? newcandidate then
+        check = true
+        peds[newcandidate].each do |p|
+          if !result.include? p then
+            check = false
+          end
+        end
+        if check then
+          candidates.append newcandidate
+        end
+      end
+    end
+    nextletter = candidates.min
+  end
+  result = result + nextletter
+
+  return result
+
+end
+
+input = File.read("day07_input.txt").chomp
+puts day07(input)
---
+++ day07a_spec.rb
@@ -0,0 +1,8 @@
+require "rspec"
+require_relative "day07a"
+
+describe "day07" do
+  it "returns 'CABDFE' for 'Step C must be finished before step A can begin.\nStep C must be finished before step F can begin.\nStep A must be finished before step B can begin.\nStep A must be finished before step D can begin.\nStep B must be finished before step E can begin.\nStep D must be finished before step E can begin.\nStep F must be finished before step E can begin.'" do
+    expect(day07("Step C must be finished before step A can begin.\nStep C must be finished before step F can begin.\nStep A must be finished before step B can begin.\nStep A must be finished before step D can begin.\nStep B must be finished before step E can begin.\nStep D must be finished before step E can begin.\nStep F must be finished before step E can begin.")).to eq "CABDFE"
+  end
+end
