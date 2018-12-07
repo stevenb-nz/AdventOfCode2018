b4906342a7441a3437cda46bbce3f8ed6a5f4ec1
Steven Brown <sgbrown@mac.com>
2018-12-07 20:30:56 +1300

    day 7 part B setup

---
+++ day07b_spec.rb
@@ -0,0 +1,8 @@
+require "rspec"
+require_relative "day07b"
+
+describe "day07" do
+  it "returns 15 for 'Step C must be finished before step A can begin.\nStep C must be finished before step F can begin.\nStep A must be finished before step B can begin.\nStep A must be finished before step D can begin.\nStep B must be finished before step E can begin.\nStep D must be finished before step E can begin.\nStep F must be finished before step E can begin.'" do
+    expect(day07("Step C must be finished before step A can begin.\nStep C must be finished before step F can begin.\nStep A must be finished before step B can begin.\nStep A must be finished before step D can begin.\nStep B must be finished before step E can begin.\nStep D must be finished before step E can begin.\nStep F must be finished before step E can begin.")).to eq 15
+  end
+end
