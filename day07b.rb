4954383d70ee79f2ad1d74a4cdbb456d6b69cdb6
Steven Brown <sgbrown@mac.com>
2018-12-09 21:03:06 +1300

    day 7 part B solution

--- day07b.rb
+++ day07b.rb
@@ -16,39 +16,83 @@ def day07(input,s_offset,n_workers)
     end
   end
 
-  candidates = []
-  deps.each do |k,v|
-    if v[0].empty? then
-      candidates.append k
+  workers = []
+  n_workers.times do
+    workers.push ["",0]
+  end
+  result = ""
+  minutes = -1
+
+  while result.length < deps.count
+    minutes += 1
+    workers.each_with_index do |worker,i|
+      if worker[1] <= 0 then
+        if worker[0] != "" then
+          result += worker[0]
+          workers[i][0] = ""
+        end
+      end
+    end
+    workers.each_with_index do |worker,i|
+      if worker[1] <= 0 then
+        workers[i] = assign(deps,workers,result,s_offset)
+      end
+      workers[i][1] -= 1
     end
   end
 
-  result = ""
-  nextletter = candidates.min
+  return minutes
 
-  while !deps[nextletter][1].empty?
-    result = result + nextletter
-    candidates.delete nextletter
-    deps[nextletter][1].each do |newcandidate|
-      if !result.include? newcandidate then
+end
+
+def assign(deps,workers,result,s_offset)
+  candidates = []
+  if result == "" then
+    deps.each do |k,v|
+      if v[0].empty? then
         check = true
-        deps[newcandidate][0].each do |p|
-          if !result.include? p then
+        workers.each do |w|
+          if w[0] == k then
             check = false
           end
         end
         if check then
-          candidates.append newcandidate
+          candidates.append k
         end
       end
     end
-    nextletter = candidates.min
+  else
+    result.each_char do |c|
+      deps[c][1].each do |newcandidate|
+        if !result.include? newcandidate then
+          check = true
+          workers.each do |w|
+            if w[0] == newcandidate then
+              check = false
+            end
+          end
+          if check then
+            deps[newcandidate][0].each do |p|
+              if !result.include? p then
+                check = false
+              end
+            end
+          end
+          if check then
+            candidates.append newcandidate
+          end
+        end
+      end
+    end
   end
-  result = result + nextletter
 
-  return result
-
+  if candidates.empty? then
+    return ["",0]
+  else
+    duration = candidates.min.ord - 64 + s_offset
+    return [candidates.min,duration]
+  end
 end
 
-#input = File.read("day07_input.txt").chomp
-#puts day07(input,60,5)
+input = File.read("day07_input.txt").chomp
+puts day07(input,60,5)
