5a815c17c74b9ebb2eea06bb0dac17e8f8c1db45
Steven Brown <sgbrown@mac.com>
2019-01-02 02:16:50 +1300

    refactor

--- day15a.rb
+++ day15a.rb
@@ -5,16 +5,16 @@ def day15(input)
     row = row.split(//)
     map.push row
   end
+
+
   units = []
   map.each_with_index do |row,y|
     row.each_with_index do |square,x|
       if square != '#' and square != '.' then
         units.push [x,y,square]
-        map[y][x] = '.'
       end
     end
   end
-  units.sort_by { |e| [e[1],e[0]] }
 
   return 27730
 end
